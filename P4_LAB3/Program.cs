4a94272e81cad413ae815a5fa77bfb6556d80dd8
makka999 <55660660+makka999@users.noreply.github.com>
2022-04-09 19:00:54 +0200

    wykonanie zadania

---
+++ P4_LAB3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+namespace P4_LAB3
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var cstring = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind2;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var conn = new SqlConnection(cstring);
+
+            var regionToInsert = new Region()
+            {
+                RegionID = 9,
+                RegionDescription = "polska"
+            };
+
+
+            var joinResult = conn.Query<Territories, Region, Territories>("SELECT * FROM Territories t JOIN Region r ON t.RegionID = r.RegionID WHERE TerritoryDescription LIKE 'B%'",
+                (territories, region) =>
+                {
+                    territories.Region = region;
+                    return territories;
+                },
+                splitOn: "RegionID");
+            foreach (var item in joinResult)
+            {
+                Console.WriteLine($"{item.TerritoryDescription} {item.Region.RegionDescription}");
+            }
+        }
+    }
+}
---
+++ P4_LAB3/Territories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_LAB3
+{
+    class Territories
+    {
+        public int TerritoryID { get; set; }
+        public string TerritoryDescription { get; set; }
+        
+        public Region Region { get; set; }
+    }
+}
